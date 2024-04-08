985911aa9ce109603e427a42386ac32346cbf342
Javier Martínez <143547061+Javierjmc@users.noreply.github.com>
2024-04-08 21:20:07 +0000

    insertando los componentes de marxel

--- project-marxel/src/App.tsx
+++ project-marxel/src/App.tsx
@@ -1,26 +1,59 @@
+import { useState } from "react"
+
+
+
+const CARD:number=0
+const ACCOUNT:number=1
+const PAGINATION:number=2
+const SHOP:number=3
+const TEL:number=4
+
 function App() {
 
-  const CARD:number=0
-  const ACCOUNT:number=1
-  const PAGINATION:number=2
-  const SHOP:number=3
-  const TEL:number=4
+  const [tab,setTab]=useState<number>(CARD)
 
+  const handlerNav=(selectTab:number)=>{
+    return ()=>{
+      setTab(selectTab)
+    }
+  }
   
-
   return (
     <div className="h-screen flex flex-col items-center">
       <nav className=" mt-5 border border-dotted border-[#EC4E20] min-w-[500px] sm:min-w-[650px]">
         <ul className="flex justify-between"> 
-          <li className="border-r border-dotted border-[#EC4E20] w-full  py-1 text-xl font-semibold text-center hover:bg-slate-300 transition-all duration-700 cursor-pointer">Card</li>
-          <li className="border-r border-dotted border-[#EC4E20] w-full  py-1 text-xl font-semibold text-center hover:bg-slate-300 transition-all duration-700 cursor-pointer">Acount</li>
-          <li className="border-r border-dotted border-[#EC4E20] w-full py-1 text-xl font-semibold text-center hover:bg-slate-300 transition-all duration-700 cursor-pointer">Pagination</li>
-          <li className="border-r border-dotted border-[#EC4E20] w-full py-1 text-xl font-semibold text-center hover:bg-slate-300 transition-all duration-700 cursor-pointer">Shop</li>
-          <li className="border-r border-dotted border-[#EC4E20] w-full py-1 text-xl font-semibold text-center hover:bg-slate-300 transition-all duration-700 cursor-pointer">Tel</li>
+          <li 
+            onClick={handlerNav(CARD)}
+            className={tab===CARD? "bg-slate-300 border-r border-dotted border-[#EC4E20] w-full  py-1 text-xl font-semibold text-center cursor-pointer" : "border-r border-dotted border-[#EC4E20] w-full  py-1 text-xl font-semibold text-center hover:bg-slate-300 transition-all duration-700 cursor-pointer"}>
+            CARD
+          </li>
+          <li 
+            onClick={handlerNav(ACCOUNT)}
+            className={tab===ACCOUNT? "bg-slate-300 border-r border-dotted border-[#EC4E20] w-full  py-1 text-xl font-semibold text-center cursor-pointer" : "border-r border-dotted border-[#EC4E20] w-full  py-1 text-xl font-semibold text-center hover:bg-slate-300 transition-all duration-700 cursor-pointer"}>
+            ACCOUNT
+          </li>
+          <li 
+            onClick={handlerNav(PAGINATION)}
+            className={tab===PAGINATION? "bg-slate-300 border-r border-dotted border-[#EC4E20] w-full  py-1 text-xl font-semibold text-center cursor-pointer" : "border-r border-dotted border-[#EC4E20] w-full  py-1 text-xl font-semibold text-center hover:bg-slate-300 transition-all duration-700 cursor-pointer"}>
+            PAGINTION
+          </li>
+          <li onClick={handlerNav(SHOP)} 
+            className={tab===SHOP? "bg-slate-300 border-r border-dotted border-[#EC4E20] w-full  py-1 text-xl font-semibold text-center cursor-pointer" : "border-r border-dotted border-[#EC4E20] w-full  py-1 text-xl font-semibold text-center hover:bg-slate-300 transition-all duration-700 cursor-pointer"}>
+            SHOP
+          </li>
+          <li 
+            onClick={handlerNav(TEL)} 
+            className={tab===TEL? "bg-slate-300 border-r border-dotted border-[#EC4E20] w-full  py-1 text-xl font-semibold text-center cursor-pointer" : "border-r border-dotted border-[#EC4E20] w-full  py-1 text-xl font-semibold text-center hover:bg-slate-300 transition-all duration-700 cursor-pointer"}>
+            TEL
+          </li>
         </ul>
-      </nav>          
+      </nav> 
+
+      <section>
+        {tab}
+      </section>        
     </div> 
   )
-}
+} 
 
 export default App
